1643941f37ae4cfd255e60abc28bd0cc8e7f1339
tonik <xkisa@stuba.sk>
2022-03-02 21:37:22 +0100

    zodiac sign 1

---
+++ src/main/java/opp/ulohy/Main.java
@@ -0,0 +1,15 @@
+package opp.ulohy;
+
+import opp.ulohy.utility.Zklavesnice;
+
+public class Main {
+    public static void main()
+    {
+        System.out.println("Napis datum narodenia...");
+        int day = Zklavesnice.readInt("Den: ");
+        int month = Zklavesnice.readInt("Mesiac: ");
+
+
+
+    }
+}
