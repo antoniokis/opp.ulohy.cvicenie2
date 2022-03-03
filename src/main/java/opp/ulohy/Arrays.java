d638d7aaacd7ad2803c085140c5d21e308ede66f
tonik <xkisa@stuba.sk>
2022-03-03 11:56:54 +0100

    vsetky metody v array class

--- src/main/java/opp/ulohy/Arrays.java
+++ src/main/java/opp/ulohy/Arrays.java
@@ -24,4 +24,37 @@ public class Arrays {
             System.out.print(i + " ");
         }
     }
+
+    public static int[] arrayInitialisation()
+    {
+        return new int[]{1, 3, 5, 7, 8};
+    }
+
+    public static int[] sortArray(int[] array)
+    {
+        int min;
+        int tempIndex = 0;
+        boolean foundNewMin = false;
+        for (int i = 0; i < array.length; i++)
+        {
+            min = array[i];
+            for (int j = i; j < array.length; j++)
+            {
+                if (min > array[j])
+                {
+                    min = array[j];
+                    tempIndex = j;
+                    foundNewMin = true;
+                }
+            }
+            if (foundNewMin)
+            {
+                int temp = array[i];
+                array[i] = min;
+                array[tempIndex] = temp;
+                foundNewMin = false;
+            }
+        }
+        return array;
+    }
 }
--- src/main/java/opp/ulohy/Main.java
+++ src/main/java/opp/ulohy/Main.java
@@ -1,12 +1,8 @@
 package opp.ulohy;
 
 import opp.ulohy.utility.Zklavesnice;
-
-import static opp.ulohy.Arrays.printArrayContent;
-import static opp.ulohy.InitialisationArray.arrayInitialisation;
-import static opp.ulohy.Sorting.sortArray;
+import static opp.ulohy.Arrays.*;
 import static opp.ulohy.Zodiac.getZodiac;
-import static opp.ulohy.Arrays.addToArray;
 
 public class Main {
     public static void main(String[] args)
